c522b468c01b60d466a34ca6326dda4887b14a2a
RabbiDoy <1048484658@qq.com>
2024-05-27 16:21:52 +0800

    跳转实现5.27.16：21

--- app/src/main/java/com/example/ktv_system/MainActivity.java
+++ app/src/main/java/com/example/ktv_system/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mbtfadanmu;
-
     private Button mbtfazhufu;
     private Button mbtgequfenlei;
     private Button mbtgexing;
     private Button mbtyaokong;
     private Button mbtdiange;
 
+    private GridView mlvgequ_mingdan;
+    private ListView mgvgexing_mingdan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,23 +30,56 @@ public class MainActivity extends AppCompatActivity {
         mbtgexing = findViewById(R.id.gexing);
         mbtyaokong = findViewById(R.id.yaokong);
         mbtdiange = findViewById(R.id.diange);
+        mlvgequ_mingdan = findViewById(R.id.gequ_mingdan);
+        mgvgexing_mingdan = findViewById(R.id.gexing_mingdan);
         Intent intentfadanmu = new Intent();
         Intent intentfazhufu = new Intent();
-        Intent intentgequfenlei = new Intent();
-        Intent intentgexing = new Intent();
         Intent intentyaokong = new Intent();
         Intent intentdiange = new Intent();
         intentfadanmu.setAction("redirecttofadanmu");
         intentfadanmu.addCategory(Intent.CATEGORY_DEFAULT);
         intentfazhufu.setAction("redirecttofazhufu");
         intentfazhufu.addCategory(Intent.CATEGORY_DEFAULT);
-        intentgequfenlei.setAction("redirecttogequfennlei");
-        intentgequfenlei.addCategory(Intent.CATEGORY_DEFAULT);
-        intentgexing.setAction("redirecttogexing");
-        intentgexing.addCategory(Intent.CATEGORY_DEFAULT);
         intentyaokong.setAction("redirecttoyaokong");
         intentyaokong.addCategory(Intent.CATEGORY_DEFAULT);
         intentdiange.setAction("redirecttodiange");
         intentdiange.addCategory(Intent.CATEGORY_DEFAULT);
+        mbtfadanmu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentfadanmu);
+            }
+        });
+        mbtfazhufu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentfazhufu);
+            }
+        });
+        mbtyaokong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentyaokong);
+            }
+        });
+        mbtdiange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentdiange);
+            }
+        });
+        mbtgequfenlei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+       mbtgexing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
