a3beb895961b62254376e5f0b7b5a7f00de3b501
RabbiDoy <1048484658@qq.com>
2024-05-27 14:22:26 +0800

    TEST

--- app/src/main/java/com/example/ktv_system/MainActivity.java
+++ app/src/main/java/com/example/ktv_system/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.ktv_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mbt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
