5d029cd0d95680205c2a5fead90e1379927e68d3
2921382045 <2921382045@qq.com>
2024-05-27 14:24:07 +0800

    创建了四个歌曲分类界面

---
+++ app/src/main/java/com/example/ktv_system/GequLiuxingActivity.java
@@ -0,0 +1,14 @@
+package com.example.ktv_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GequLiuxingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gequ_liuxing);
+    }
+}
--- app/src/main/java/com/example/ktv_system/GequYaogunActivity.java
+++ app/src/main/java/com/example/ktv_system/GequYaogunActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GequActivity extends AppCompatActivity {
+public class GequYaogunActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gequ);
+        setContentView(R.layout.activity_gequ_yaogun);
     }
 }
