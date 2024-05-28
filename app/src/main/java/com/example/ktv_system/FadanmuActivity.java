a9a3a30c2ed038a02e0e6373fe828614d02cc57a
2921382045 <2921382045@qq.com>
2024-05-28 14:03:06 +0800

    实现了发弹幕界面的设计。

--- app/src/main/java/com/example/ktv_system/FadanmuActivity.java
+++ app/src/main/java/com/example/ktv_system/FadanmuActivity.java
@@ -3,9 +3,13 @@ package com.example.ktv_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class FadanmuActivity extends AppCompatActivity {
 
+
+
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
