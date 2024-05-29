5d8503ec94ba5ec3e56b81361d3228a59effca3c
2921382045 <2921382045@qq.com>
2024-05-29 11:19:10 +0800

    完善了发弹幕，发祝福除了视频之外的功能

--- app/src/main/java/com/example/ktv_system/FadanmuActivity.java
+++ app/src/main/java/com/example/ktv_system/FadanmuActivity.java
@@ -2,12 +2,29 @@ package com.example.ktv_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.ToggleButton;
 import android.widget.VideoView;
 
 public class FadanmuActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    private EditText editText;
+    private ToggleButton btn1;
+    private ToggleButton btn2;
+    private ToggleButton btn3;
+    private ToggleButton btn4;
+
+
+
 
 
     @Override
@@ -15,5 +32,55 @@ public class FadanmuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fadanmu);
 
+        videoView=findViewById(R.id.shiping);
+        editText= findViewById(R.id.fandanmu_et);
+        btn1=findViewById(R.id.fadanmu_btn1);
+        btn2=findViewById(R.id.fandanmu_btn2);
+        btn3=findViewById(R.id.fandanmu_btn3);
+        btn4=findViewById(R.id.fandanmu_btn4);
+
+        MediaController mediaController=new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn3.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("狂击6666~");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1.setChecked(false);
+                btn3.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("听你的歌，真的需要勇气");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn1.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("有毛病呀，唱这么好听干嘛");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn3.setChecked(false);
+                btn1.setChecked(false);
+                editText.setText("想要的都拥有，得不到的都释怀。生日快乐");
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/ktv_system/FazhufuActivity.java
+++ app/src/main/java/com/example/ktv_system/FazhufuActivity.java
@@ -3,12 +3,71 @@ package com.example.ktv_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+import android.widget.VideoView;
 
 public class FazhufuActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    private EditText editText;
+    private ToggleButton btn1;
+    private ToggleButton btn2;
+    private ToggleButton btn3;
+    private ToggleButton btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fazhufu);
+
+        videoView=findViewById(R.id.shipin);
+        editText=findViewById(R.id.fabianzhufu);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn3.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("祝你生日快乐");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1.setChecked(false);
+                btn3.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("愿我们的友情地久天长");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn1.setChecked(false);
+                btn4.setChecked(false);
+                editText.setText("愿我们的爱情如夏日的阳光，温暖而明亮，照亮彼此的生命之路");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                btn2.setChecked(false);
+                btn3.setChecked(false);
+                btn1.setChecked(false);
+                editText.setText("岁月悠悠，又迎新年，祝我们新年快乐，心想事成，步步高升");
+            }
+        });
+
     }
 }
