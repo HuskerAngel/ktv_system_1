1aab6c235f99e2989dd8b498d9484e6f72f7a347
2921382045 <2921382045@qq.com>
2024-05-30 14:31:38 +0800

    实现了视频播放的功能

--- app/src/main/java/com/example/ktv_system/FazhufuActivity.java
+++ app/src/main/java/com/example/ktv_system/FazhufuActivity.java
@@ -3,15 +3,22 @@ package com.example.ktv_system;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
 import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.DrawableMarginSpan;
@@ -27,15 +34,29 @@ import android.widget.VideoView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
 import java.util.Random;
 
 import master.flame.danmaku.controller.DrawHandler;
+import master.flame.danmaku.controller.IDanmakuView;
+import master.flame.danmaku.danmaku.loader.ILoader;
+import master.flame.danmaku.danmaku.loader.IllegalDataException;
+import master.flame.danmaku.danmaku.loader.android.DanmakuLoaderFactory;
 import master.flame.danmaku.danmaku.model.BaseDanmaku;
 import master.flame.danmaku.danmaku.model.DanmakuTimer;
 import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.IDisplayer;
+import master.flame.danmaku.danmaku.model.android.BaseCacheStuffer;
 import master.flame.danmaku.danmaku.model.android.DanmakuContext;
 import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.model.android.SpannedCacheStuffer;
 import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.danmaku.parser.IDataSource;
+import master.flame.danmaku.danmaku.parser.android.BiliDanmukuParser;
+import master.flame.danmaku.danmaku.util.IOUtils;
 import master.flame.danmaku.ui.widget.DanmakuView;
 
 public class FazhufuActivity extends AppCompatActivity {
@@ -47,13 +68,11 @@ public class FazhufuActivity extends AppCompatActivity {
     private ToggleButton btn2;
     private ToggleButton btn3;
     private ToggleButton btn4;
-    private DanmakuView danmaku;
+    private IDanmakuView mDanmakuView;
     private boolean showDanmaku;
-    private DanmakuContext danmakuContext;
-    private Context context;
+    private DanmakuView danmaku;
 
-    private TextView textView;
-    private ImageView imageView;
+    private DanmakuContext danmakuContext;
     private Button btn;
 
 
@@ -63,57 +82,36 @@ public class FazhufuActivity extends AppCompatActivity {
             return new Danmakus();
         }
     };
-    /**
-     * 创建图文混排模式
-     * @param drawable
-     * @return
-     */
-    private SpannableStringBuilder createSpannable(Drawable drawable) {
-        //25dp转化为px
-        float dp = 25;
-        final float scale = getResources().getDisplayMetrics().density;
-        //由25dp转化来的px
-        int px = (int) (dp * scale + 0.5f);
-
-
-
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("小明回复小红：你在干嘛呀。");
-        //添加图片
-        drawable.setBounds(0, 0, px, px);
-        ImageSpan imageSpan1 = new ImageSpan(drawable);
-        spannableString.setSpan(imageSpan1, spannableString.length() - 1, spannableString.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        return spannableString;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fazhufu);
 
-        videoView=findViewById(R.id.shipin);
-        editText=findViewById(R.id.fabianzhufu);
-        btn1=findViewById(R.id.btn1);
-        btn2=findViewById(R.id.btn2);
-        btn3=findViewById(R.id.btn3);
-        btn4=findViewById(R.id.btn4);
-        btn=findViewById(R.id.fazhufu_fason);
-        danmaku=findViewById(R.id.fazhufu_danmaku);
+        videoView = findViewById(R.id.shipin);
+        editText = findViewById(R.id.fabianzhufu);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn = findViewById(R.id.fazhufu_fason);
+        mDanmakuView = findViewById(R.id.fazhufu_danmaku);
+;
+        danmakuContext = DanmakuContext.create();
 
-//        initDanmaku();
+        initDanmaku();
 
 
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        ImageSpan imageSpan = new ImageSpan(this, R.mipmap.ic_launcher);
-        //也可以这样
-        //Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
-        //drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-        //ImageSpan imageSpan1 = new ImageSpan(drawable);
-        //将index为6、7的字符用图片替代
-        spannableString.setSpan(imageSpan, 6, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
 
-        textView.setText(spannableString);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.shipin2);
+        videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);//让视频循环播放
+            }
+        });
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -161,16 +159,15 @@ public class FazhufuActivity extends AppCompatActivity {
                 btn1.setChecked(false);
                 btn4.setChecked(false);
 
-                View view = LayoutInflater.from(FazhufuActivity.this).inflate(R.layout.activity_duihuankuan,null);
-                AlertDialog.Builder builder=new AlertDialog.Builder(FazhufuActivity.this);//要改view的背景颜色，需要再values加一个styles。(MainActivity2.this,R.style.dialog)通过一个资源文件来得到一个圆角
+                View view = LayoutInflater.from(FazhufuActivity.this).inflate(R.layout.activity_duihuankuan, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(FazhufuActivity.this);//要改view的背景颜色，需要再values加一个styles。(MainActivity2.this,R.style.dialog)通过一个资源文件来得到一个圆角
                 builder.setView(view);
                 builder.create().show();
 
 
-
                 String content = editText.getText().toString();
-                if (!TextUtils.isEmpty(content)){
-//                    addDanmakuImage(content,true);     // 添加一条弹幕
+                if (!TextUtils.isEmpty(content)) {
+                    addDanmaku(content, true);    // 添加一条弹幕
                     editText.setText("");
                 }
 
@@ -181,14 +178,12 @@ public class FazhufuActivity extends AppCompatActivity {
     }
 
 
-
-
     private void initDanmaku(){
         danmaku.setCallback(new DrawHandler.Callback() {
             @Override
             public void prepared() {
                 showDanmaku = true;
-                danmaku.start();    // 开始弹幕
+                danmaku.start();    // 开始弹幕// 随机生成弹幕的方法
             }
             @Override
             public void updateTimer(DanmakuTimer timer) {
@@ -209,52 +204,30 @@ public class FazhufuActivity extends AppCompatActivity {
         // 弹幕view准备  --- 传入解析器
         danmaku.prepare(parser,danmakuContext);
 
-    }
 
 
-//    private void addDanmaku(String content, boolean border) {
-//        // 创建弹幕对象，设置从右向左滚动
-//        BaseDanmaku baseDanmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
-//        baseDanmaku.text = content;     // 设置内容
-//        baseDanmaku.padding = 6;        // 设置边距
-//        baseDanmaku.textSize = 25;      // 设置弹幕字体大小
-//        baseDanmaku.textColor = Color.WHITE;
-//        baseDanmaku.time=danmaku.getCurrentTime();// 设置弹幕字体颜色
-//        // 设置当前时间
-//        if (border){
-//            baseDanmaku.borderColor = Color.BLUE;       // 设置边框颜色
-//        }
-//        // 添加弹幕至弹幕视图组件中
-//        danmaku.addDanmaku(baseDanmaku);
-//    }
-
-
-    // 添加弹幕
-//    private void addDanmakuImage(String content, boolean islive) {
-//        // 创建弹幕对象，设置从右向左滚动
-//        BaseDanmaku baseDanmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
-//
-//        baseDanmaku.text = spannableString;     // 设置内容
-//        baseDanmaku.padding = 6;        // 设置边距
-//        baseDanmaku.priority=1;
-//        baseDanmaku.isLive=islive;
-//        baseDanmaku.textSize = 25;      // 设置弹幕字体大小
-//        baseDanmaku.textColor = Color.WHITE;
-//        baseDanmaku.textShadowColor=0;
-//        baseDanmaku.time=danmaku.getCurrentTime()+1200;// 设置弹幕字体颜色
-//       // 设置当前时间
-//        if (islive){
-//            baseDanmaku.borderColor = Color.BLUE;       // 设置边框颜色
-//        }
-//        // 添加弹幕至弹幕视图组件中
-//        danmaku.addDanmaku(baseDanmaku);
-//    }
-
-    // 随机生成弹幕
 
 
     }
+    private void addDanmaku(String content, boolean border) {
+        // 创建弹幕对象，设置从右向左滚动
+        BaseDanmaku baseDanmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
+        baseDanmaku.text = content;     // 设置内容
+        baseDanmaku.padding = 6;        // 设置边距
+        baseDanmaku.textSize = 25;      // 设置弹幕字体大小
+        baseDanmaku.textColor = Color.WHITE;
+        baseDanmaku.time=danmaku.getCurrentTime();// 设置弹幕字体颜色
+        // 设置当前时间
+        if (border){
+            baseDanmaku.borderColor = Color.BLUE;       // 设置边框颜色
+        }
+        // 添加弹幕至弹幕视图组件中
+        danmaku.addDanmaku(baseDanmaku);
+    }
 
+
+
+}
 
 
 
