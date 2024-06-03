3ea04c567586becfe23dda5c3f775229441c459e
Return <3189880277@qq.com>
2024-06-03 21:25:44 +0800

    发祝福监听器独立化

---
+++ app/src/test/java/com/example/ktv_system/gej/FazhufuActivityTest.java
@@ -0,0 +1,52 @@
+package com.example.ktv_system.gej;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FazhufuActivityTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onbtn4click() {
+    }
+
+    @Test
+    public void onbtn3click() {
+    }
+
+    @Test
+    public void onbtn2click() {
+    }
+
+    @Test
+    public void onbtn1click() {
+    }
+
+    @Test
+    public void onuploadclick() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+}
