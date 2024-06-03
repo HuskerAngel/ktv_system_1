a5f567da166bdc47d818a04307725d4e5a7dfc9b
RabbiDoy <1048484658@qq.com>
2024-06-03 16:25:47 +0800

    分包

---
+++ app/src/test/java/com/example/ktv_system/dr/ControlFragmentTest.java
@@ -0,0 +1,31 @@
+package com.example.ktv_system.dr;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ControlFragmentTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testNewInstance() {
+    }
+
+    public void testOnCreate() {
+    }
+
+    public void testOnCreateView() {
+    }
+
+    public void testOnViewCreated() {
+    }
+
+    public void testOnResume() {
+    }
+
+    public void testOnDestroy() {
+    }
+}
+
